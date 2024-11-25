3c3a49aec54dc5613d94050fe6bb9a994ba29d3f
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-25 11:59:14 +0100

    fix main in abuot.jsx

--- react-libreria/src/pages/about.jsx
+++ react-libreria/src/pages/about.jsx
@@ -3,10 +3,10 @@ export default function Home() {
     return (
         <>
 
-            <main>
-                <h1>About</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum laudantium doloremque aliquid similique commodi dolor veritatis animi! Nihil, mollitia.</p>
-            </main>
+
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum laudantium doloremque aliquid similique commodi dolor veritatis animi! Nihil, mollitia.</p>
+
 
 
         </>
