db3ee5429adee6a69fb89f8876f131a6ed893277
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-25 10:29:45 +0100

    routes

---
+++ react-libreria/src/pages/about.jsx
@@ -0,0 +1,25 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/Books">Books</a>
+                    <a href="/Contacts">Contacts</a>
+                    <a href="/About">About</a>
+                </nav>
+            </header>
+            <main>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum laudantium doloremque aliquid similique commodi dolor veritatis animi! Nihil, mollitia.</p>
+            </main>
+
+            <footer>
+                <p>copyrigth 2024</p>
+            </footer>
+        </>
+    )
+}
